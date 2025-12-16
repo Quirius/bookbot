ef532db31b1bf5cebf3edebb3defd313dce66fb0
Quirius <szogever@gmail.com>
2025-12-16 21:13:52 +0100

    1.1 - added input requirement if no filepath is given as an argument

--- main.py
+++ main.py
@@ -3,14 +3,15 @@ from stats import count_words, get_book_text, count_characters, sort_characters
 
 def main():
     if len(sys.argv) != 2:
-        print("Usage: python3 main.py <path_to_book>")
-        sys.exit(1)
-    book_text = get_book_text(sys.argv[1])
+        filepath = input("Enter the path to the book file: ")
+    else:
+        filepath = sys.argv[1]
+    book_text = get_book_text(filepath)
     num_words = count_words(book_text)
     char_count = count_characters(book_text)
     sorted_chars = sort_characters(char_count)
     print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {sys.argv[1]}...")
+    print(f"Analyzing book found at {filepath}...")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
