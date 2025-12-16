8a9c7d5ef7c66acbb5b9947f4386a5a3218393ea
Quirius <szogever@gmail.com>
2025-12-16 21:00:44 +0100

    1.0 - added compatibility for multiple books

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
+import sys
 from stats import count_words, get_book_text, count_characters, sort_characters
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_text = get_book_text(sys.argv[1])
     num_words = count_words(book_text)
     char_count = count_characters(book_text)
     sorted_chars = sort_characters(char_count)
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {sys.argv[1]}...")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
