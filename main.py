b70f911c620a6a2624c577698c43011f46475fc3
Quirius <szogever@gmail.com>
2025-12-16 20:50:18 +0100

    new functions

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-from stats import count_words, get_book_text
+from stats import count_words, get_book_text, count_characters, sort_characters
 
 def main():
     book_text = get_book_text("books/frankenstein.txt")
     num_words = count_words(book_text)
+    char_count = count_characters(book_text)
+    sorted_chars = sort_characters(char_count)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in sorted_chars:
+        print(f"{item['char']}: {item['num']}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -4,3 +4,23 @@ def get_book_text(filepath):
 
 def count_words(text):
     return len(text.split())
+
+def count_characters(text):
+    char_count = {}
+    for char in text.lower():
+        if not char.isalpha():
+            continue
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    return char_count
+
+def sort_characters(char_count):
+    def get_num(item):
+        return item["num"]
+
+    list_of_dicts = [{"char": char, "num": count} for char, count in char_count.items()]
+    list_of_dicts.sort(key=get_num, reverse=True)
+    return list_of_dicts
+
